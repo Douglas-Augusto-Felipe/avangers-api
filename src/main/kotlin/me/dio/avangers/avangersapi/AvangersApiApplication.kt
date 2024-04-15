9bbf225c3d7e15259cb57c13ac7dd0455d423d7b
Douglas Augusto <douglas.full.ti@gmail.com>
2024-04-15 02:24:12 -0300

    Initial commit

---
+++ src/main/kotlin/me/dio/avangers/avangersapi/AvangersApiApplication.kt
@@ -0,0 +1,11 @@
+package me.dio.avangers.avangersapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvangersApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AvangersApiApplication>(*args)
+}
---
+++ src/test/kotlin/me/dio/avangers/avangersapi/AvangersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.dio.avangers.avangersapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvangersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
